1766e73bd9d56ce031e0318787753c83bd60db97
Daniel Elstner <daniel.kitta@gmail.com>
2010-06-02 07:40:35 +0200

    Simplify autogen.sh and enable verbose output
    * autogen.sh: Pass the source directory as an argument to autoreconf
    instead of invoking a sub-shell.  Also turn on verbose output.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,7 @@
 #! /bin/sh -e
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
-(
-  cd "$srcdir" &&
-  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
-) || exit
+
+AUTOPOINT='intltoolize --automake --copy' \
+autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
